75c1f3ac926e7672c0933291fac53a4ea70c9565
Stella <stellamutacho@gmail.com>
2022-07-16 22:22:21 +0300

    modifications on the Smart contract code

--- refund_contract/contracts/Refund.sol
+++ refund_contract/contracts/Refund.sol
@@ -2,8 +2,98 @@
 pragma solidity >=0.6.0 <0.9.0;
 
 contract Refund {
-    address employer;          //the owner of the contract
-    uint256 private constant RESOLUTION = 1000000000000000; //To be used when calculating coordinates 
+    address owner;          //the creator of the contract
+    int256 private constant RESOLUTION = 1000000000000000;  //to be used as the denominator when calculating the coordinates
+    uint256 private constant RADIUS =100;         //the maximum possible allowed radius around the exact location where the employee is expected to be
+    address[] employee_list;      //A list of employee. Employees are identified by their public key addresses
+
+    mapping(address => Employee_Info) public contractInfo;
+
+    struct Coordinate {
+        uint256 lat;
+        uint256 long;
+    }                   //represents a location on a map
+
+    struct Employee_Info{
+        Coordinate expected_location;       //location where the employee is expected to checkin
+        uint budget;                        //the amount the employee is supposed to be reimbursed if he/she is compliant with the contract
+        bool isCompliant;        //true if the employee is compliant with the contract
+        uint count;             //keeps track of number of times the employee has been checked to comply with the contract
+    }
+    /*
+    This Function is the constructor which registers the creator of the contract.
+    */
+    constructor() {
+        owner = msg.sender;
+    }
+    /*
+    Function that terminates the contract if the employer initializes end of the contract
+    */
+    function kill() public {
+      if(msg.sender == owner) selfdestruct(payable(owner));
+    }
+/**
+*Function to check if employee is in the list of employees
+*@param empAddress Public key of the employee
+*@return true if employee is in the list and false otherwise
+*/
+    function checkEmployeeExistance(address empAddress) private view returns(bool){ 
+    for(uint256 i = 0; i < employee_list.length; i++){ 
+        if(employee_list[i] == empAddress)
+        return true;  
+    }        
+    return false;
+    }
+
+/**
+*Function to add new employee details in the list
+*@param id Public key of the employee
+*@param lat Latitude of employee location on map
+*@param lon longitude of employee location on map
+*@param budget amount to be reimbursed by the Employer
+*/
+    function addEmployee(address id, uint256 lat, uint256 lon, uint8 budget) public  {
+    require(!checkEmployeeExistance(id));
+    contractInfo[id].expected_location = Coordinate(lat, lon);
+    contractInfo[id].budget = budget;
+    contractInfo[id].isCompliant = true;
+    employee_list.push(id);
+    }
+/**
+ *Calculates the square root of an integer
+ *@param x The integer to get it square root
+ *@return y the square root of the integer
+ */
+    function sqrt(uint x) private pure returns (uint y) {
+    uint z = (x + 1) / 2;
+        y = x;
+        while (z < y) {
+            y = z;
+            z = (x / z + z) / 2;
+        }
+    } 
+
+    function getDistance(uint256 lat, uint256 lon, address _employeeAddress) private view returns(uint256) {
+        uint256 x = lat - contractInfo[_employeeAddress].expected_location.lat;
+            uint256 y = lon - contractInfo[_employeeAddress].expected_location.long;
+            uint256 radius = sqrt(x**2 + y**2);
+            return radius;
+    }
+    function checkDistance(uint256 lat, uint256 lon) public {
+        uint256 new_radius = getDistance(lat, lon, msg.sender);
+        if(new_radius < RADIUS){
+            contractInfo[msg.sender].isCompliant = true;
+        }else{
+            contractInfo[msg.sender].isCompliant = false;
+        }
+    }
+    function reimburse(address payable _to) public {
+            require(checkEmployeeExistance(_to));
+            require(contractInfo[_to].isCompliant == true);
+            bool sent = _to.send(contractInfo[_to].budget);
+            require(sent, "Failed reimburse the employee!!");
+            contractInfo[_to].isCompliant=false;
+        }
     
 
     event Approval(address indexed tokenOwner, address indexed spender, uint tokens);
@@ -12,93 +102,8 @@ contract Refund {
     mapping(address => uint256) balances;
     mapping(address => mapping (address => uint256)) allowed;
     
-    struct Coordinates{   //representation of the location on a map
-        int longitude; 
-        int latitude;    
-    }                      
-
-    struct Employee_info{
-        address employee_address;   //Public address of the employee
-        Coordinates expected_location; //The exact area the employee should be around
-        bool isCompliant;     //True is the employee is with the range 
-        uint amount;            //amount to transfer to the employee if compliant with the contract
-        uint count;
-        int256 radius;             //keeps track of number of times the employee has been checked to comply with the contract
-    }
-    Employee_info[] employee_list; //list of employees with their details
-
-    /*
-    This Function is the constructor which registers the creator of the contract.
-    */
-    constructor() {
-        employer = msg.sender;
-    }
-    /*
-    Function that terminates the contract if the employer initializes end of the contract
-    */
-    function kill() public {
-      if(msg.sender == employer) selfdestruct(payable(employer));
-    }
-    function abs(int x) private pure returns (int) {
-    return x >= 0 ? x : -x;
-    }
-/**
-*Function to check if employee is in the list of employees
-*@param empAddress Public key of the employee
-*@return true if employee is in the list and false otherwise
-*/
-    function checkEmployeeExistance(address empAddress) private view returns(bool){
-        for(uint256 i = 0; i < employee_list.length; i++){
-            if(employee_list[i].employee_address == empAddress)
-            return true;
-            }
-            return false;
-            }
-/**
-*Function to add new employee details in the list
-*@param id Public key of the employee
-*@param lat Latitude of employee location on map
-*@param lon longitude of employee location on map
-*@param rad distance within which employee is allowed by the employer 
-*@param budget amount to be reimbursed by the Employer
-*/
-    function add_employee(address id, int256 lat, int256 lon, int256 rad, uint8 budget) public{
-        require(!checkEmployeeExistance(id));
-        Coordinates memory expected_loc = Coordinates(lat, lon);
-        Employee_info memory employee = Employee_info(id, expected_loc, true, budget,  0, rad);
-        employee_list.push(employee);
-    }
-    
-    function sqrt(int256 input) private pure returns (int256 output) {
-        int256 interim = (input + 1) / 2;
-        output = input;
-        while (interim < output) {
-            output = interim;
-            interim = (input / output * 2) / 2;
-        }
-    }
-    function calculate_radius(int256 lat, int256 lon, Employee_info memory _employee) private pure returns(int256) {
-        int256 radius = 0;
-        int256 x = lat - _employee.expected_location.latitude;
-        int256 y = lon - _employee.expected_location.longitude;
-        radius = sqrt(x**2 + y**2);
-        return radius;
-    }
-    function check_position(int256 lat, int256 lon) public {
-        int256 new_radius = calculate_radius(lat, lon, msg.sender);
-        if(new_radius < contractInfo[msg.sender].radius){
-            employeeInfo[msg.sender].status = true;
-        }else{
-            employeeInfo[msg.sender].status = false;
-        }
-    }
-
-    //Function to enforce the contract only if the employees location is within a given radius
-    // function get_location(Employee memory _employee)public returns(mapping(address=>Coordinates) employee_location)
-   
-
-
-    //Function to transfer money from employer to employee 
-   
-
 }
+
+    
+    
+    
